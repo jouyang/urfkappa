a294c8c90a8cb170626aa8dea7dc9375f7f463fe
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2015-04-14 14:09:38 -0700

    fix structure

---
+++ data_aggregator/BucketGenerator.py
@@ -0,0 +1,36 @@
+import requests
+import key
+from time import gmtime
+
+required_payload = {'api_key': key.api_key()}
+root_url = "https://na.api.pvp.net/"
+
+def merge_dicts(*dict_args):
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+class BucketGenerator:
+	challenge_api_suffix = "api/lol/na/v4.1/game/ids"
+	begin_slot = 1427865900
+	current_slot = 1427865900
+	challenge_payload = {}
+
+	def __init__(self):
+		self.challenge_payload["beginDate"] = self.begin_slot
+
+	def increment_time_slot(self):
+		self.current_slot += 300
+		self.challenge_payload["beginDate"] = self.current_slot
+
+	def reset_time_slot(self):
+		self.current_slot = self.begin_slot
+		self.challenge_payload["beginDate"] = self.current_slot
+
+	#Returns list of game id's
+	def get_bucket(self):
+		req = requests.get(root_url + self.challenge_api_suffix, params=merge_dicts(required_payload, self.challenge_payload))
+		self.increment_time_slot()
+		return req.text
+
