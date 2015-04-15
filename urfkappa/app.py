570a48ed1f2d11d71a2e7fed754fddcca3361329
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2015-04-14 23:56:33 -0700

    reorganization

--- urfkappa/app.py
+++ urfkappa/app.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-	json_data = open("./champ_data.txt", "r")
+	json_data = open("champ_data.json", "r")
 	data = json.load(json_data)
 	return render_template('index.html', champ_list=data.items())
 
