cda2f6b38c6f7043143350cf39f1319bb6d56498
Jeffrey Ouyang <jeffreyouyang5293@gmail.com>
2015-04-15 01:26:35 -0700

    added ending time

--- data_aggregator/BucketGenerator.py
+++ data_aggregator/BucketGenerator.py
@@ -1,6 +1,7 @@
 import requests
 import key
 import ast
+import time
 
 required_payload = {'api_key': key.api_key()}
 root_url = "https://na.api.pvp.net/"
@@ -15,6 +16,7 @@ class BucketGenerator:
 	challenge_api_suffix = "api/lol/na/v4.1/game/ids"
 	begin_slot = 1427865900
 	current_slot = 1427865900
+	end_slot = 1428890400
 	challenge_payload = {}
 
 	def __init__(self):
@@ -28,9 +30,17 @@ class BucketGenerator:
 		self.current_slot = self.begin_slot
 		self.challenge_payload["beginDate"] = self.current_slot
 
+	def reach_end(self):
+		return (self.current_slot >= self.end_slot)
+
 	#Returns list of game id's
 	def get_bucket(self):
 		req = requests.get(root_url + self.challenge_api_suffix, params=merge_dicts(required_payload, self.challenge_payload))
+
+		while(req.status_code !=200):
+			time.sleep(10)
+			req = requests.get(root_url + self.challenge_api_suffix, params=merge_dicts(required_payload, self.challenge_payload))
+
 		self.increment_time_slot()
 		game_ids = [item for item in ast.literal_eval(req.text)]
 		return game_ids
--- data_aggregator/UrfDataAggregator.py
+++ data_aggregator/UrfDataAggregator.py
@@ -35,81 +35,87 @@ class UrfDataAggregator:
 		return items
 
 	def get_match_data(self):
-		while(not self.current_bucket):
-			self.update_bucket()
-		
-		match_id = self.current_bucket.pop()
-		req = requests.get(root_url + str(match_id), params=required_payload)
-
-		while(req.status_code !=200):
-			time.sleep(10)
+		while(True):
+			if(self.bucket.reach_end()):
+				return
+			else:
+				time.sleep(1)
+				
+			while(not self.current_bucket):
+				self.update_bucket()
+			
+			match_id = self.current_bucket.pop()
 			req = requests.get(root_url + str(match_id), params=required_payload)
 
-		reqJson = req.json()
-		players = reqJson['participants']
-		if (reqJson['queueType'] == u"URF_5x5"):
-			team1_ChampID = []
-			team2_ChampID = []
-			team1_pnum_to_champ = {}
-			team2_pnum_to_champ = {}
-			p1_item = []
-			p2_item = []
-			p3_item = []
-			p4_item = []
-			p5_item = []
-			p6_item = []
-			p7_item = []
-			p8_item = []
-			p9_item = []
-			p10_item = []
-			winner = "100"
-			for p in players:
-				
-				if (p['teamId'] == 100):
-					team1_ChampID.append(p['championId'])
-					team1_pnum_to_champ[p['participantId']] = p['championId']
-				elif (p['teamId'] == 200):
-					team2_ChampID.append(p['championId'])
-					team2_pnum_to_champ[p['participantId']] = p['championId']
-				
-				if (p['participantId'] ==  1):
-						p1_item = self.extract_items(p['stats'])
-						if (not p['stats']['winner']):
-							winner = "200"
-				elif (p['participantId'] ==  2):
-						p2_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  3):
-						p3_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  4):
-						p4_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  5):
-						p5_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  6):
-						p6_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  7):
-						p7_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  8):
-						p8_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  9):
-						p9_item = self.extract_items(p['stats'])
-				elif (p['participantId'] ==  10):
-						p10_item = self.extract_items(p['stats'])
-			team1_ChampID = str(team1_ChampID)
-			team2_ChampID = str(team2_ChampID)
-			team1_pnum_to_champ = str(team1_pnum_to_champ)
-			team2_pnum_to_champ = str(team2_pnum_to_champ)
-			p1_item = str(p1_item)
-			p2_item = str(p2_item)
-			p3_item = str(p3_item)
-			p4_item = str(p4_item)
-			p5_item = str(p5_item)
-			p6_item = str(p6_item)
-			p7_item = str(p7_item)
-			p8_item = str(p8_item)
-			p9_item = str(p9_item)
-			p10_item = str(p10_item)
-			m = matches.matches(team1_ChampID, team2_ChampID, p1_item, p2_item, p3_item, p4_item, p5_item, p6_item, p7_item, p8_item, p9_item, p10_item, winner, team1_pnum_to_champ, team2_pnum_to_champ)
-			app.db.session.add(m)
-			app.db.session.commit()
+			while(req.status_code !=200):
+				time.sleep(10)
+				req = requests.get(root_url + str(match_id), params=required_payload)
+
+			reqJson = req.json()
+			players = reqJson['participants']
+			if (reqJson['queueType'] == u"URF_5x5"):
+				team1_ChampID = []
+				team2_ChampID = []
+				team1_pnum_to_champ = {}
+				team2_pnum_to_champ = {}
+				p1_item = []
+				p2_item = []
+				p3_item = []
+				p4_item = []
+				p5_item = []
+				p6_item = []
+				p7_item = []
+				p8_item = []
+				p9_item = []
+				p10_item = []
+				winner = "100"
+				for p in players:
+					
+					if (p['teamId'] == 100):
+						team1_ChampID.append(p['championId'])
+						team1_pnum_to_champ[p['participantId']] = p['championId']
+					elif (p['teamId'] == 200):
+						team2_ChampID.append(p['championId'])
+						team2_pnum_to_champ[p['participantId']] = p['championId']
+					
+					if (p['participantId'] ==  1):
+							p1_item = self.extract_items(p['stats'])
+							if (not p['stats']['winner']):
+								winner = "200"
+					elif (p['participantId'] ==  2):
+							p2_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  3):
+							p3_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  4):
+							p4_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  5):
+							p5_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  6):
+							p6_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  7):
+							p7_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  8):
+							p8_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  9):
+							p9_item = self.extract_items(p['stats'])
+					elif (p['participantId'] ==  10):
+							p10_item = self.extract_items(p['stats'])
+				team1_ChampID = str(team1_ChampID)
+				team2_ChampID = str(team2_ChampID)
+				team1_pnum_to_champ = str(team1_pnum_to_champ)
+				team2_pnum_to_champ = str(team2_pnum_to_champ)
+				p1_item = str(p1_item)
+				p2_item = str(p2_item)
+				p3_item = str(p3_item)
+				p4_item = str(p4_item)
+				p5_item = str(p5_item)
+				p6_item = str(p6_item)
+				p7_item = str(p7_item)
+				p8_item = str(p8_item)
+				p9_item = str(p9_item)
+				p10_item = str(p10_item)
+				m = matches.matches(team1_ChampID, team2_ChampID, p1_item, p2_item, p3_item, p4_item, p5_item, p6_item, p7_item, p8_item, p9_item, p10_item, winner, team1_pnum_to_champ, team2_pnum_to_champ)
+				app.db.session.add(m)
+				app.db.session.commit()
 
 
